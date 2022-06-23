94bf1393d3f328f5d84247546dcb19b192b5f7f8
amadou <amadoukeita00@gmail.com>
2022-06-23 15:40:30 +0200

    Initialisation register

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image,  Alert, Button, Vibration } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Navbar from './pages/navbar';
+import Login from './pages/Login';
 
 export default function App() {
     
   return (
     <NavigationContainer>
-      <Navbar />
+      {/* <Navbar/> */}
+      <Login/>
     </NavigationContainer>
   );
 }
